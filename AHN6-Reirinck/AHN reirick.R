d8ae49829b011f108a787b299098f181a8506ef2
Mathijs Keijzer <mathijskeijzer744@gmail.com>
2026-05-26 10:45:37 +0200

    refined tree segmentation and added tree level statistics

--- AHN6-Reirinck/AHN reirick.R
+++ AHN6-Reirinck/AHN reirick.R
@@ -31,17 +31,16 @@ ahn_small <- clip_roi(ahn, bb_union)
 # visualize
 plot(ahn_small)
 
-# optional: save clipped file
-writeLAS(ahn_small, "part2/AHN6_clipped.laz")
+
 
 
 
 ################simple plots#####################
 # first we classify the ground
 ahn_ground <- classify_ground(ahn_small, csf())  # Cloth Simulation Filter
- 
-dtm <- rasterize_terrain(ahn_ground, res = 1, algorithm = tin())
-dsm <- rasterize_canopy(ahn_small, res = 1, algorithm = p2r())
+
+dtm <- rasterize_terrain(ahn_ground, res = 0.5, algorithm = tin())
+dsm <- rasterize_canopy(ahn_small, res = 0.5, algorithm = p2r())
 
 #plotting side by side
 par(mfrow=c(1,2))
@@ -64,20 +63,64 @@ add_treetops3d(x, treetops)
 # replace `yourCRvalue` with a value:
 # segmenting trees using silva2016 algorithm
 trees_silva2016 <- segment_trees(ahn_small, silva2016(chm, treetops, max_cr_factor= 0.7, ID="treeID"), attribute = "treeID")
-#trees_silva2016 <- segment_trees(ahn_small, silva2016values, attribute = "treeID")
 # plotting
 plot(trees_silva2016, color = "treeID")
 # summary of the number of trees found.
 summary(trees_silva2016@data[["treeID"]])
 
-
-# segmenting trees using dalponte2016 algorithm
-trees_dalponte2016 <- segment_trees(ahn_small, dalponte2016(chm, treetops), attribute = "treeID")
-# plotting
-plot(trees_dalponte2016, color = "treeID")
-# summary of the number of trees found.
-summary(trees_dalponte2016@data[["treeID"]])
-
-visualizedtreeID <- filter_poi(trees_dalponte2016, treeID == 111)
+visualizedtreeID <- filter_poi(trees_silva2016, treeID == 111)
 plot(visualizedtreeID, size = 6, bg = "white")
+
+#################################################
+# TREE-LEVEL STATISTICS
+#################################################
+
+# 1. Normalize the point cloud using the DTM generated earlier
+# This ensures Z coordinates represent height above ground, not elevation
+trees_normalized <- normalize_height(trees_silva2016, dtm)
+
+# remove unsegmented points
+trees <- filter_poi(trees_normalized, !is.na(treeID))
+
+# compute metrics
+tree_stats <- crown_metrics(
+  trees,
+  func = ~list(
+    
+    # heights
+    height_max  = max(Z),
+    
+    # point count
+    n_points = length(Z),
+    
+    # crown dimensions
+    crown_radius = max(dist(cbind(X, Y))) / 2,
+    crown_diameter = max(dist(cbind(X, Y))),
+    
+    # approximate crown volume
+    crown_volume = pi *
+      (max(dist(cbind(X, Y))) / 2)^2 *
+      max(Z) * 0.5
+  ),
+  geom = "convex"
+)
+
+#################################################
+# VIEW RESULTS
+#################################################
+
+print(tree_stats)
+
+summary(tree_stats)
+
+#################################################
+# EXPORT
+#################################################
+
+write.csv(
+  sf::st_drop_geometry(tree_stats),
+  "tree_statistics.csv",
+  row.names = FALSE
+)
+
 
